a9740402f81e6a6bb082e4c8784ba09229a25eb2
OGUNLEYE AYODAMOLA <ogunleyedamola1995@yahoo.com>
2023-12-20 00:01:34 +0100

    added new files

--- bank/src/main/java/io/digisic/bank/repository/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package io.digisic.bank.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import io.digisic.bank.model.security.UserRole;
-
-public interface UserRoleRepository extends CrudRepository<UserRole, Long> {
-
-}
--- credit/src/main/java/io/digisic/credit/repository/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package io.digisic.credit.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import io.digisic.credit.model.security.UserRole;
-
-public interface UserRoleRepository extends CrudRepository<UserRole, Long> {
-
-}
